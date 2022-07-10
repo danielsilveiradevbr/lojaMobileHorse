9ed7b7ffcb083dda6e5ee54dd9ecdef8e6924e9b
daniel <danielzinhors@gmail.com>
2022-07-10 16:14:58 -0300

    Colocando icone Tela Login

--- src/views/Views.Principal.pas
+++ src/views/Views.Principal.pas
@@ -5,12 +5,15 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.Edit, FMXLabelEdit;
+  FMX.Controls.Presentation, FMX.Edit, FMXLabelEdit, FMX.Layouts;
 
 type
   TfrmPrincipal = class(TForm)
+    StyleBook: TStyleBook;
+    lylContent: TLayout;
+    procedure FormActivate(Sender: TObject);
   private
-    { Private declarations }
+    FActivate: boolean;
   public
     { Public declarations }
   end;
@@ -21,5 +24,19 @@ var
 implementation
 
 {$R *.fmx}
+
+uses Views.Login;
+
+procedure TfrmPrincipal.FormActivate(Sender: TObject);
+var
+  LFrmLogin: TFrmLogin;
+begin
+  if FActivate then
+    exit;
+  LFrmLogin := TFrmLogin.create(lylContent);
+  LFrmLogin.Align := TAlignLayout.Client;
+  lylContent.AddObject(LFrmLogin);
+  FActivate := true;
+end;
 
 end.
